b7f17948abf9c38122516fb2077b6d4591ed7df7
Ha jun Lim <58055835+BakeCoCo@users.noreply.github.com>
2022-05-13 10:38:16 +0900

    Delete practiceRMI directory

--- practiceRMI/src/main/java/test/rmi/RSInterface.java
+++
@@ -1,8 +0,0 @@
-package test.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-//
-public interface RSInterface extends Remote {
-    public void println(String msg) throws RemoteException;
-}
